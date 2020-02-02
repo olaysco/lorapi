351afb397f5ac92d7e2e251cf3d839926a707df3
olaysco <olayiwolaodunsi@gmail.com>
2020-02-02 14:32:35 +0100

    create /movie endpoint and implemented movie controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers;
 
+setlocale(LC_MONETARY, 'en_NG');
+
 use Illuminate\Http\Request;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
+use Illuminate\Support\Arr;
 
 class MovieController extends Controller
 {
     public $movieClient;
-    public function all()
+    static $sortKeys = ["asc" => true, "desc" => true];
+
+    public function movies()
     {
         $this->movieClient = new Client(['base_uri' => 'https://the-one-api.herokuapp.com']);
         $result = $this->movieClient->get('v1/movie', [
@@ -19,6 +24,52 @@ class MovieController extends Controller
             ]
         ]);
         $allMovies = json_decode($result->getBody()->getContents());
-        return response()->json($allMovies, 200);
+        return $allMovies;
+    }
+
+    public function all(Request $request)
+    {
+        $budget = $request->budget;
+        $runtime = $request->runtime;
+        $boxRevenue = $request->boxOfficeRevenue;
+        return $this->sortBy($this->buildSortingCriteria($budget, $runtime, $boxRevenue));
+    }
+
+    public function sortBy(array $sortingCriteria)
+    {
+        $moviesCollection = collect($this->movies()->docs);
+        foreach ($sortingCriteria as $criteria => $order) {
+            if ($order === 'asc') {
+                $moviesCollection = $moviesCollection->sortBy($criteria);
+            } else {
+                $moviesCollection = $moviesCollection->sortByDesc($criteria);
+            }
+        }
+        $moviesCollection->map(function ($movie) {
+            $budgetNaira = ((int) $movie->budgetInMillions)  * 350 * 1000000;
+            $boxOfficeRevenueNaira = ((int) $movie->boxOfficeRevenueInMillions)  * 350 * 1000000;
+            $movie->budgetInNaira = number_format($budgetNaira);
+            $movie->boxOfficeRevenueNaira = number_format($boxOfficeRevenueNaira);
+            unset($movie->budgetInMillions);
+            unset($movie->boxOfficeRevenueInMillions);
+        });
+        return response()->json($moviesCollection, 200);
+    }
+
+    public function buildSortingCriteria($budget = "", $runtime = "", $boxRevenue = "")
+    {
+        $sortingCriteria = [];
+
+        if (!empty($budget) && Arr::has(self::$sortKeys, $budget)) {
+            $sortingCriteria["budgetInMillions"] = $budget;
+        }
+        if (!empty($runtime) && Arr::has(self::$sortKeys, $runtime)) {
+            $sortingCriteria["runtimeInMinutes"] = $runtime;
+        }
+        if (!empty($boxRevenue) && Arr::has(self::$sortKeys, $boxRevenue)) {
+            $sortingCriteria["boxOfficeRevenueInMillions"] = $boxRevenue;
+        }
+
+        return $sortingCriteria;
     }
 }
